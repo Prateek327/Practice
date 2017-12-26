18bd290c719110ea71a5cbd605af3b0adae81be6
Prateek Khandelwal <prateekkhandelwal.327@gmail.com>
2017-12-26 21:08:35 +0530

    Added Program

---
+++ sieveoferasthones.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+main()
+{
+    int prime[1000],i,j,n;
+    printf("\nIn order to find prime numbers from 1 to n, enter the value of n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        prime[i]=1;
+    }
+    prime[1]=0;
+    for(i=2;i*i<=n;i++)
+    {
+             /* multi-threading to remove multiples of prime number i from the list (array) */
+
+
+            for(j=i*i;j<=n;j=j+i)
+              {
+                  if(prime[j]==1)
+                       prime[j]=0;
+              }
+
+    }
+    printf("\nPrime numbers from 1 to %d are\n",n);
+
+    for(i=2;i<=n;i++)
+    {
+        if(prime[i] == 1)
+        {
+            printf("%d\t ",i);
+        }
+    }
+    printf("\n");
+}
